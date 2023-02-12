d7a4d3ae8bb7a1cd81ad0ddaa7bac7d262df1184
Gauhar <gauharbegimova@gmail.com>
2023-02-13 04:50:51 +0600

    fix: code style clean whitespace

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -9,32 +9,17 @@ import java.util.Map;
 
 public class ItemMapper {
     public static ItemDto toItemDto(Item item) {
-        return new ItemDto(
-                item.getId(),
-                item.getName(),
-                item.getDescription(),
-                item.getIsAvailable(),
-                item.getRequestId());
+        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getIsAvailable(), item.getRequestId());
     }
 
     public static Item toItem(ItemDto itemDto) {
-        return new Item(null,
-                itemDto.getName(),
-                itemDto.getDescription(),
-                itemDto.getAvailable(),
-                itemDto.getRequestId(),
-                null);
+        return new Item(null, itemDto.getName(), itemDto.getDescription(), itemDto.getAvailable(), itemDto.getRequestId(), null);
     }
 
     public static Collection<ItemDto> toItemDtoCollection(Map<Integer, Item> itemMap) {
         Collection<ItemDto> newCollection = new ArrayList<>();
         for (Item item : itemMap.values()) {
-            ItemDto itemDto = new ItemDto(
-                    item.getId(),
-                    item.getName(),
-                    item.getDescription(),
-                    item.getIsAvailable(),
-                    item.getRequestId());
+            ItemDto itemDto = new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getIsAvailable(), item.getRequestId());
 
             newCollection.add(itemDto);
         }
