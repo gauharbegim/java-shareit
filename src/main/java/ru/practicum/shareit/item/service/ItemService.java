9ae27918161f091517e850861f3863164a496d4a
Gauhar <gauharbegimova@gmail.com>
2023-02-14 17:03:55 +0600

    fix: code style clean whitespace

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -5,13 +5,13 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.Collection;
 
 public interface ItemService {
-    ItemDto addItem(Integer ownerId, ItemDto itemDto);
+    ItemDto addItem (Integer ownerId, ItemDto itemDto);
 
-    ItemDto update(Integer ownerId, Integer itemId, ItemDto itemDto);
+    ItemDto update (Integer ownerId, Integer itemId, ItemDto itemDto);
 
-    ItemDto getItem(Integer itemId);
+    ItemDto getItem (Integer itemId);
 
-    Collection<ItemDto> getItems(Integer ownerId);
+    Collection<ItemDto> getItems (Integer ownerId);
 
-    Collection<ItemDto> getItems(String text);
+    Collection<ItemDto> getItems (String text);
 }
